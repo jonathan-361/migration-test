b6b3715ebb58a04aa3cfbcc7d1882f88efce7b8c
jonathan-361 <jonathanlopezuc@gmail.com>
2023-11-12 20:47:33 -0600

    Se subio la prueba

---
+++ database/migrations/2023_11_13_022015_create_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('publication_year');
+            $table->foreignId('author_id')->constrained('authors');
+            $table->foreignId('genre_id')->constrained('genres');
+            $table->integer('available_quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
